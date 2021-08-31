dd42becbac0dfc7a5223a885f8d314ad47d9199f
avishekataur <avishekataur@gmail.com>
2021-08-31 17:03:46 +0600

    bind-method

--- object-binding.js
+++ object-binding.js
@@ -1,8 +1,10 @@
 /////////////// node object-binding.js ///////////////
-const book =
+
+/////////////// book(1) ///////////////
+const firstBook =
 {
-    id: 123-456-222, 
-    title: 'The Ultimate Database Study Guide',  
+    id: 123, 
+    title: 'Database Study Guide',  
     category: ['on-Fiction', 'Technology'],
     author: 
     {
@@ -20,7 +22,59 @@ const book =
     buy: function (discount) 
     {
         this.price = this.price - discount;
-
+        console.log('All-Books',this); //Bind Method from this
         return this.price;
-    },
+    }
 }
+/////////////// book(2) ///////////////
+const secondBook =
+{
+    id: 345, 
+    title: 'Design Study Guide',  
+    category: ['Colors', 'Arts'],
+    author: 
+    {
+      lastname: 'Jaden',
+      firstname: 'Smith'
+    },
+        
+    price: 3500
+    
+   
+}
+/////////////// book(2) ///////////////
+const thirdBook =
+{
+    id: 567, 
+    title: 'Finance Study Guide',  
+    category: ['Currency', 'Banks'],
+    author: 
+    {
+      lastname: 'Tahmina',
+      firstname: 'Akter'
+    },
+        
+    price: 1500
+    
+   
+}
+
+/* 
+firstBook.buy(200);
+firstBook.buy(350);
+*/
+firstBook.buy(500); 
+
+
+const buySecond = firstBook.buy.bind(secondBook);
+/* 
+buySecond(300);
+buySecond(700); 
+*/
+buySecond(700);
+
+const buyThird = firstBook.buy.bind(thirdBook);
+
+buyThird(250);
+
+//Tutorial(2) = 35Mins
